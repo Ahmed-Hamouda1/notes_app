550f330f347078ccb2efcc645d154a404f43840c
AhmedHamouda <ahmedhamooda221@gmail.com>
2025-04-28 15:28:43 +0300

    create app bar

---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/notes_view_appbar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding
+    (
+      padding: const EdgeInsets.all(15),
+      child: Column
+      (
+        children: 
+        [
+          SizedBox(height: 50,),
+          NotesViewAppBar()
+        ],
+      ),
+    );
+  }
+}
